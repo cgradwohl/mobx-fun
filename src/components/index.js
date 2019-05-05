fec528ff154f023d3908120b079436b0b953ec74
Chris Gradwohl <christophergradwohl@gmail.com>
2019-05-05 08:53:33 -0700

    changes name of exported lib to gradwohl.min.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,10 @@
+/**
+ * This file is here in case you want to use this
+ * repo as a component library. You can import
+ * export the component here and then export them
+ * in src/inde.js. Webpack will bundle each
+ * exported component using code splitting.
+ */
 import App from './App/App';
 
 export {
